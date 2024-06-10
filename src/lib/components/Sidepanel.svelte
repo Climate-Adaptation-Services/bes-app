e6cbd9073520917c3257c2fde02fdcc94011bef1
sophievanderhorst17 <sophie_vanderhorst@hotmail.com>
2024-06-10 10:00:28 +0200

    Update Sidepanel.svelte

--- src/lib/components/Sidepanel.svelte
+++ src/lib/components/Sidepanel.svelte
@@ -95,7 +95,7 @@
     <img class = 'countrylogo bo activecountry' id = 'Bonaire' src="https://raw.githubusercontent.com/sophievanderhorst/data/main/bonairenew.png" on:click={handleClickCountry}> 
     <figcaption class = 'countrycaptionbo'>Bonaire</figcaption>
     <img class = 'countrylogo se' id = 'st.Eustatius & Saba' src="https://raw.githubusercontent.com/sophievanderhorst/data/main/saba_eus_gezelligbijelkaar.png" on:click={handleClickCountry}> 
-    <figcaption class = 'countrycaptionse'>st. Eustatius & Saba</figcaption>    
+    <figcaption class = 'countrycaptionse'>Saba & st. Eustatius </figcaption>    
 </section>
 
 <style>
